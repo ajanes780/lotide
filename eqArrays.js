bb7efb1be472818e2e4a27aa5256bdea71355860
ajanes780 <aaron.janes@gmail.com>
2021-01-06 02:24:04 +0000

    Checks arrays

---
+++ eqArrays.js
@@ -0,0 +1,43 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    if (actual !== expected) {
+      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    }
+  }
+};
+
+
+
+
+const eqArrays = function(a, b) {
+  
+  if (a.length !== b.length) {
+    console.log("false ");
+  } else {
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] !== b[i]) {
+        console.log(" false ");
+        return;
+      } else {
+        console.log(" true");
+        return;
+      }
+    }
+
+  }
+};
+
+
+
+
+// eqArrays([1, 2, 3], [1, 2, 3, ]) // => true
+// eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+// eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+// eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3, ]), true); // => should PASS
