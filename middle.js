39ce52d74246dad5a04912a2a9c493bc37d11da8
Aaron Janes <Aaron.Janes@gmail.com>
2021-01-11 16:59:04 -0700

    refactoring

--- middle.js
+++ middle.js
@@ -1,46 +1,5 @@
-const assertArraysEqual = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return console.log(`🛑 These arrays are not the same length`);
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return console.log(`❌ ${array1} !== ${array2}`);
-    }
-  }
-  return console.log(`✅ Assertion passed ${array1} === ${array2}`);
-};
-
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-//   } else {
-//     if (actual !== expected) {
-//       console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-//     }
-//   }
-// };
-
-
-
-const eqArrays = function(a, b) {
-  
-  if (a.length !== b.length) {
-    
-    return assertEqual(1,5);
-  } else {
-    for (let i = 0; i < a.length; i++) {
-      if (a[i] !== b[i]) {
-        
-        return assertEqual(1,5);
-      } else {
-        
-        return assertEqual(1,1);
-      }
-    }
-
-  }
-};
-
+const assertArraysEqual = require(`../lotide/assertArraysEqual`)
+const eqArrays = require(`../lotide/eqArrays`)
 
 //Find the mmiddle numbers
 // there is no middle for even lenegth arrays
@@ -48,31 +7,30 @@ const eqArrays = function(a, b) {
 
 
 
-const middle = function(data) {
-  let middleNumbers = [];
-  if (data.length <=  2)
-    return [];
-
-  const midindex =  Math.floor(data.length / 2);
-
-  if (data.length % 2 !== 0) {
-    middleNumbers.push(data[midindex]);
-  } else {
-    middleNumbers.push(data[midindex - 1 ]);
-    middleNumbers.push(data[midindex]);
-    return middleNumbers;
+const middle = function(arrayToMiddle) {
+  let index = [];
+  let emptyArray = [];
+  if (arrayToMiddle.length <= 2) {
+    return emptyArray;
+  } else if (arrayToMiddle.length % 2 !== 0) {
+    index.push(Math.round(arrayToMiddle.length / 2));
+    return index;
+  } else if (arrayToMiddle.length % 2 === 0) {
+    index.push(arrayToMiddle.length / 2,arrayToMiddle.length / 2 + 1);
+    return index;
   }
 };
 
 
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+
+
+  
+
 
 assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+assertArraysEqual(middle([1]), [],false);
+assertArraysEqual(middle([1, 2]), [], false);
+assertArraysEqual(middle([1, 2, 3]), [2], true);
+assertArraysEqual(middle([1, 2, 3, 4, 5]),[3],true);
+assertArraysEqual(middle([1, 2, 3, 4]),[2, 3],true);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3, 4], true );
