5a341323fea42044744110e46bd2b3330ca58697
Aaron Janes <Aaron.Janes@gmail.com>
2021-01-11 18:09:20 -0700

    refactoring mocha

--- middle.js
+++ middle.js
@@ -1,27 +1,35 @@
-const assertArraysEqual = require(`../lotide/assertArraysEqual`)
+
 const eqArrays = require(`../lotide/eqArrays`)
 
 //Find the mmiddle numbers
 // there is no middle for even lenegth arrays
 // check lenghth  then divid by 2
 
+// @maria Chavez
 
 
-const middle = function(arrayToMiddle) {
-  let index = [];
-  let emptyArray = [];
-  if (arrayToMiddle.length <= 2) {  /// if the arry is less then two return []
-    return emptyArray;
-  } else if (arrayToMiddle.length % 2 !== 0) { // if its not a even length 
-    index.push(Math.round(arrayToMiddle.length / 2));
-    return index;
-  } else if (arrayToMiddle.length % 2 === 0) { // if it is a even length 
-    index.push(arrayToMiddle.length / 2,arrayToMiddle.length / 2 + 1);
-    return index;
+const middle = function (array) {
+  let middleIndex = [];
+  //identify array’s length and
+  //if array has one or two elements, return empty array
+  if (array.length <= 2) {
+    middleIndex = [];
+    //if array has odd number of elements return single middle %2 === 0 > false > length / 2
+  } else if (array.length % 2 !== 0) {
+    middleIndex = [array[Math.round(array.length / 2) - 1]];
+    //if array has even number of elements return 2 elements from middle %2 === 0 > true > length / 2 + 1
+  } else if (array.length % 2 === 0) {
+    // const middleOne = array[Math.round((array.length / 2) - 1)]
+    // const middleTwo = array[Math.round(array.length / 2)]
+    middleIndex = [
+      array[Math.round(array.length / 2 - 1)],
+      array[Math.round(array.length / 2)],
+    ];
   }
+  return middleIndex;
 };
 
-
+// console.log(middle([2,3,4,5,6]));
 
 
 
--- test/middleTest.js
+++ test/middleTest.js
@@ -1,12 +1,24 @@
 const middle =  require(`../middle`);
-const assertArraysEqual = require(`../assertArraysEqual`)
+const assert = require('chai').assert;
+
+
+describe("#middle", () => {
+  it("returns [2] for [1,2,3]", () => {
+    assert.deepEqual(middle([1,2,3]),[2]);
+  });
+  it("returns [] for [5]", () => {
+    assert.deepEqual(middle([5]),[]); 
+  });
+  it("returns [3,4] for [1,2,3,4,5,6] ", () => {
+    assert.deepEqual(middle([1,2,3,4,5,6]),[3,4])
+  });
+
+});
 
 
 
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([1]), [],false);
-assertArraysEqual(middle([1, 2]), [], false);
-assertArraysEqual(middle([1, 2, 3]), [2], true);
-assertArraysEqual(middle([1, 2, 3, 4, 5]),[3],true);
-assertArraysEqual(middle([1, 2, 3, 4]),[2, 3],true);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3, 4], true );
+
+
+
+
+
